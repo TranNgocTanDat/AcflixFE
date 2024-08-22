e0da2ce98c7d982d6189ae5f83fc456972774294
Tran Ngoc Tan Dat <21130314@st.hcmuaf.edu.vn>
2024-08-23 00:06:25 +0700

    update

--- src/Layout/Header/Header.tsx
+++ src/Layout/Header/Header.tsx
@@ -1,22 +1,32 @@
-
-import HeaderLast from "./HeaderLast.tsx";
-import HeaderLogo from "./HeaderLogo.tsx";
-import HeaderNavbar from "./HeaderNavbar.tsx";
+import { useEffect } from "react";
+import HeaderLast from "./HeaderLast";
+import HeaderLogo from "./HeaderLogo";
+import HeaderNavbar from "./HeaderNavbar";
 import "./style.css";
 
 const Header = () => {
+  // Fixed Header khi scroll
+  useEffect(() => {
+    const handleScroll = () => {
+      const header = document.querySelector<HTMLElement>(".header");
+      if (header) {
+        header.classList.toggle("active", window.scrollY > 100);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <>
-      <div className="header">
-        
-        <HeaderLogo />
-
-        <HeaderNavbar />
-
-        <HeaderLast />
-      </div>
-
-    </>
+    <div className="header">
+      <HeaderLogo />
+      <HeaderNavbar />
+      <HeaderLast />
+    </div>
   );
 };
 
